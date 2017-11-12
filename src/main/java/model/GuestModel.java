b46bcb9f3a76ee7cfbbf9d6ba903bee13e906649
jaguuuar <sirkamson@gmail.com>
2017-11-12 07:53:38 +0100

    Delete unused method

--- src/main/java/model/GuestModel.java
+++ src/main/java/model/GuestModel.java
@@ -25,15 +25,4 @@ public class GuestModel {
         return messageDate;
     }
 
-    @Override
-    public String toString() {
-
-        String guestString = "<p style=\"background-color:powderblue\"><p1><b>" + this.message + "</b></p1><br>" +
-                "<p2>Name: </p2>" + "<p3><b>" + this.name + "</b></p3><br>" +
-                "<p4>Date: </p4>" + "<p5>" + this.messageDate + "<p/5></p>";
-
-        return  guestString;
-    }
-
-
 }
